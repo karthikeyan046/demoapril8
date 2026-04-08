08966bed91540344b63442be29f9faffac755caa
karthikeyan046 <karthikeyan046@yahoo.co.in>
2026-04-08 08:56:25 +0530

    Add files via upload

---
+++ bank_demo.py
@@ -0,0 +1,15 @@
+# Simulate account operations
+
+def view_balance():
+    print("Your balance for the acoount is")
+
+def transfer_funds(amount):
+    print(f"Transferred ${amount} successfully!")
+
+def pay_bill(amount):
+    print(f"Bill of ${amount} paid successfully!")
+
+if __name__ == "__main__":
+    view_balance()
+    transfer_funds(1000)
+    pay_bill(200)
